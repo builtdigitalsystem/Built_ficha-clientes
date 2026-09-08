3239e29c2449d867f479e9e720531a17fe1cdcb3
builtdigitalsystem <built.digitalsystem@gmail.com>
2026-09-08 15:59:43 -0500

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import FichaCliente from "./FichaCliente.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <FichaCliente />
-  </React.StrictMode>
-);
